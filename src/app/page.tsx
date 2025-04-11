8e3669d7642b70f7a3563a1d289c740dd90764f3
Paranat Klinhom <paranat.klinhom@MacBook-Pro-khxng-Admin.local>
2025-04-11 22:06:00 +0700

    ✨ (page.tsx): change router method from replace to push for better navigation experience on 404 page redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.replace("/404");
+    router.push("/404");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
